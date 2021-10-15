d10b5bde010b2a34083e164b7037e087e9e18eb0
flashbLack98 <npalakuttige98@icloud.com>
2021-10-15 18:13:10 +0200

    .

---
+++ script.js
@@ -0,0 +1,30 @@
+let num = Math.ceil(Math.random() * 100);
+console.log(num);
+
+if (num % 2 === 0) {
+    console.log(num + "pari");
+} else {
+    console.log(num + 1 + "dispari");
+}
+
+
+
+/* Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi all’array che ha meno elementi
+ fino a quando ne avrà tanti quanti l’altro. */
+
+
+const array1 = [15, 2, 7];
+const array2 = [5];
+
+
+//inserire elementi nel secondo array
+//inserire elementi per il n di elementi del primo array
+
+
+for (i = 0; i < array1.length - 1; i++) {
+
+    (array2.push(num));
+}
+console.log(num);
+console.log(array2);;
